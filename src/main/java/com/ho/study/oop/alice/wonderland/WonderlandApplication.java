89d9875385007ac1bb064b81ce0bf102b803534e
hshwang <hshwang@fxgear.net>
2023-04-07 22:02:09 +0900

    작업 공간 생성

---
+++ src/main/java/com/ho/study/oop/alice/wonderland/WonderlandApplication.java
@@ -0,0 +1,13 @@
+package com.ho.study.oop.alice.wonderland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonderlandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonderlandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ho/study/oop/alice/wonderland/WonderlandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ho.study.oop.alice.wonderland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonderlandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
